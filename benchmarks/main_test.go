ab9e25068a19591738a36aa18df54f00385ba7d9
Robert Lin <robert@bobheadxi.dev>
2023-01-27 00:26:46 -0800

    benchmarks: fix package tests

--- benchmarks/main_test.go
+++ benchmarks/main_test.go
@@ -2,6 +2,7 @@ package benchmarks
 
 import (
 	"flag"
+	"os"
 	"testing"
 )
 
@@ -9,4 +10,5 @@ var profile = flag.Bool("profile", false, "profile")
 
 func TestMain(t *testing.M) {
 	flag.Parse()
+	os.Exit(t.Run())
 }
