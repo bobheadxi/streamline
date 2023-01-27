324ae7dbedd6b874d0afee2cab431075e4c73333
Robert Lin <robert@bobheadxi.dev>
2023-01-27 00:03:02 -0800

    stream: test constructor accepting LineReader

---
+++ internal_test.go
@@ -0,0 +1,23 @@
+package streamline
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	t.Run("with LineReader implementation", func(t *testing.T) {
+		lr := bufio.NewReader(strings.NewReader("hello world"))
+		s := New(lr)
+		assert.Equal(t, lr, s.reader)
+	})
+
+	t.Run("without LineReader implementation", func(t *testing.T) {
+		r := strings.NewReader("hello world")
+		s := New(r)
+		assert.NotEqual(t, r, s.reader)
+	})
+}
